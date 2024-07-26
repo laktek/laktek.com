bfb794dd818a1717db2b4ccc94738393dfe600a5
Lakshan Perera <lakshan@laktek.com>
2024-07-27 06:28:34 +1000

    add og image, remove CDN assets

---
+++ js/highlight.js
@@ -0,0 +1,16 @@
+import hljs from "highlight.js/lib/core";
+import javascript from "highlight.js/lib/languages/javascript";
+import go from "highlight.js/lib/languages/go";
+import shell from "highlight.js/lib/languages/shell";
+import vim from "highlight.js/lib/languages/vim";
+import json from "highlight.js/lib/languages/json";
+
+hljs.registerLanguage("javascript", javascript);
+hljs.registerLanguage("go", go);
+hljs.registerLanguage("vim", vim);
+hljs.registerLanguage("shell", shell);
+hljs.registerLanguage("json", json);
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  hljs.highlightAll();
+});
